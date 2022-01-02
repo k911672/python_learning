f0250ec4c3d0412f35921a5f7f7a620bc2872328
k911672 <k911672@gmail.com>
2022-01-02 17:27:52 +0900

    for文応用(0図形作成）改

--- index16_18.py
+++ index16_18.py
@@ -13,9 +13,20 @@ for i in range(1,4):
 # //  00000
 # // この図形をfor文を使って出力してください。
 
+# パート１
 for i in range(1,4):
   print(" " * (3 - i), end = "")
   print("0" * (2 * i - 1))
+
+# パート２
+for i in range(0,3):
+  for j in range(i, 2):
+    print(" ", end = "")
+  for k in range(5 - 2 * i , 6):
+    print("0", end = "")
+  print("")
+
+
 
 # // 18, 図形の表示
 # //    0
@@ -25,6 +36,7 @@ for i in range(1,4):
 # //    0
 # // この図形をfor文を使って出力してください。
 
+# パート１
 for i in range(1,4):
   print(" " * (3 - i), end = "")
   print("0" * (2 * i - 1))
@@ -32,3 +44,18 @@ for i in range(1,4):
 for i in reversed(range(1,3)):
   print(" " * (3 - i), end = "")
   print("0" * (2 * i - 1))
+
+# パート２
+for i in range(0,3):
+  for j in range(i, 2):
+    print(" ", end = "")
+  for k in range(5 - 2 * i , 6):
+    print("0", end = "")
+  print("")
+
+for i in range(0,2):
+  for j in range(1 - i, 2):
+    print(" ", end = "")
+  for k in range(2 * i , 3):
+    print("0", end = "")
+  print("")
